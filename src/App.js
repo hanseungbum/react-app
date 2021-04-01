a576a8ad8e985377f662ce94264dbbb2a3786c99
hanseungbum <souq7635@naver.com>
2021-04-01 16:34:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 헬로 월드
+          Edit <code>src/App.js</code> and save to reload. 헬로 월드~~
         </p>
         <a
           className="App-link"
